d7a3cfedf41438fd2f27e0c37c7f59510bdb9607
DavidKordos <dkordos@etfos.hr>
2025-05-12 20:31:11 +0200

    Update source.c

--- source.c
+++ source.c
@@ -1,9 +1,8 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-// Definicija strukture za Film
 typedef struct {
     int id;
     char naziv[100];
@@ -12,18 +11,16 @@ typedef struct {
     int trajanje;
 } Film;
 
-// Funkcija za unos filma
 void unosFilma(FILE* file) {
     Film film;
 
-    // Unos podataka
     printf("Unesite naziv filma: ");
     fgets(film.naziv, sizeof(film.naziv), stdin);
-    film.naziv[strcspn(film.naziv, "\n")] = '\0';  // Uklanja znak novog reda na kraju naziva
+    film.naziv[strcspn(film.naziv, "\n")] = '\0';
 
     printf("Unesite zanr filma: ");
     fgets(film.zanr, sizeof(film.zanr), stdin);
-    film.zanr[strcspn(film.zanr, "\n")] = '\0';  // Uklanja znak novog reda na kraju žanra
+    film.zanr[strcspn(film.zanr, "\n")] = '\0';
 
     printf("Unesite godinu izlaska filma: ");
     scanf("%d", &film.godina);
@@ -31,20 +28,17 @@ void unosFilma(FILE* file) {
     printf("Unesite trajanje filma u minutama: ");
     scanf("%d", &film.trajanje);
 
-    // Generiraj ID
-    film.id = rand();  // Možete koristiti bilo koju funkciju za generiranje ID-a
+    film.id = rand();
 
-    // Pohrani film u datoteku
-    fseek(file, 0, SEEK_END);  // Pomakni pokazivač na kraj datoteke
+    fseek(file, 0, SEEK_END);
     fprintf(file, "%d;%s;%s;%d;%d\n", film.id, film.naziv, film.zanr, film.godina, film.trajanje);
 
-    getchar();  // Čisti buffer za kasniji unos
+    getchar();  // očisti buffer
 }
 
-// Funkcija za ispis svih filmova
 void ispisFilmove(FILE* file) {
     Film film;
-    fseek(file, 0, SEEK_SET);  // Pozicionira pokazivač na početak datoteke
+    fseek(file, 0, SEEK_SET);
 
     printf("Filmove u videoteci:\n");
     while (fscanf(file, "%d;%99[^;];%49[^;];%d;%d\n", &film.id, film.naziv, film.zanr, &film.godina, &film.trajanje) != EOF) {
@@ -52,23 +46,27 @@ void ispisFilmove(FILE* file) {
     }
 }
 
-// Funkcija za ažuriranje podataka o filmu
-void azurirajFilm(FILE* file) {
+int azurirajFilm() {
     int id, novaGodina, novoTrajanje;
     char noviNaziv[100], noviZanr[50];
     int found = 0;
 
-    printf("Unesite ID filma koji želite ažurirati: ");
+    printf("Unesite ID filma koji zelite azurirati: ");
     scanf("%d", &id);
-    getchar();  // Čisti buffer
+    getchar();
+
+    FILE* file = fopen("films.txt", "r");
+    if (file == NULL) {
+        perror("Greska pri otvaranju datoteke");
+        return 0;
+    }
 
     FILE* tempFile = fopen("temp.txt", "w");
     if (tempFile == NULL) {
-        printf("Ne mogu otvoriti privremenu datoteku!\n");
-        return;
+        perror("Greska pri otvaranju privremene datoteke");
+        fclose(file);
+        return 0;
     }
-
-    fseek(file, 0, SEEK_SET);  // Pozicionira pokazivač na početak datoteke
 
     Film film;
     while (fscanf(file, "%d;%99[^;];%49[^;];%d;%d\n", &film.id, film.naziv, film.zanr, &film.godina, &film.trajanje) != EOF) {
@@ -76,56 +74,59 @@ void azurirajFilm(FILE* file) {
             found = 1;
             printf("Unesite novi naziv filma: ");
             fgets(noviNaziv, sizeof(noviNaziv), stdin);
-            noviNaziv[strcspn(noviNaziv, "\n")] = '\0';  // Ukloni novi red
+            noviNaziv[strcspn(noviNaziv, "\n")] = '\0';
 
             printf("Unesite novi zanr filma: ");
             fgets(noviZanr, sizeof(noviZanr), stdin);
-            noviZanr[strcspn(noviZanr, "\n")] = '\0';  // Ukloni novi red
+            noviZanr[strcspn(noviZanr, "\n")] = '\0';
 
             printf("Unesite novu godinu izlaska: ");
             scanf("%d", &novaGodina);
 
             printf("Unesite novo trajanje filma: ");
             scanf("%d", &novoTrajanje);
+            getchar();
 
-            // Ažurirani podaci
             fprintf(tempFile, "%d;%s;%s;%d;%d\n", film.id, noviNaziv, noviZanr, novaGodina, novoTrajanje);
         }
         else {
-            // Kopiraj nepromijenjene filmove u privremenu datoteku
             fprintf(tempFile, "%d;%s;%s;%d;%d\n", film.id, film.naziv, film.zanr, film.godina, film.trajanje);
         }
-    }
-
-    if (!found) {
-        printf("Film s ID-em %d nije pronađen!\n", id);
     }
 
     fclose(file);
     fclose(tempFile);
 
+    if (!found) {
+        printf("Film s ID-em %d nije pronaden!\n", id);
+        remove("temp.txt");
+        return 0;
+    }
+
     remove("films.txt");
     rename("temp.txt", "films.txt");
-
-    file = fopen("films.txt", "r+");  // Ponovno otvori datoteku za kasniji rad
+    return 1;
 }
 
-// Funkcija za brisanje filma
-void obrisiFilm(FILE* file) {
-    int id;
-    int found = 0;
+int obrisiFilm() {
+    int id, found = 0;
 
-    printf("Unesite ID filma koji želite obrisati: ");
+    printf("Unesite ID filma koji zelite obrisati: ");
     scanf("%d", &id);
-    getchar();  // Čisti buffer
+    getchar();
+
+    FILE* file = fopen("films.txt", "r");
+    if (file == NULL) {
+        perror("Greska pri otvaranju datoteke");
+        return 0;
+    }
 
     FILE* tempFile = fopen("temp.txt", "w");
     if (tempFile == NULL) {
-        printf("Ne mogu otvoriti privremenu datoteku!\n");
-        return;
+        perror("Greska pri otvaranju privremene datoteke");
+        fclose(file);
+        return 0;
     }
-
-    fseek(file, 0, SEEK_SET);  // Pozicionira pokazivač na početak datoteke
 
     Film film;
     while (fscanf(file, "%d;%99[^;];%49[^;];%d;%d\n", &film.id, film.naziv, film.zanr, &film.godina, &film.trajanje) != EOF) {
@@ -137,36 +138,37 @@ void obrisiFilm(FILE* file) {
         }
     }
 
-    if (!found) {
-        printf("Film s ID-em %d nije pronađen!\n", id);
-    }
-
     fclose(file);
     fclose(tempFile);
 
+    if (!found) {
+        printf("Film s ID-em %d nije pronaden!\n", id);
+        remove("temp.txt");
+        return 0;
+    }
+
     remove("films.txt");
     rename("temp.txt", "films.txt");
-
-    file = fopen("films.txt", "r+");  // Ponovno otvori datoteku za kasniji rad
+    return 1;
 }
 
-// Glavni izbornik
 int main() {
+    
     FILE* file = fopen("films.txt", "r+");
     if (file == NULL) {
-        file = fopen("films.txt", "w+");  // Ako datoteka ne postoji, kreiraj je
+        file = fopen("films.txt", "w+");
         if (file == NULL) {
-            printf("Ne mogu otvoriti datoteku!\n");
+            perror("Ne mogu otvoriti datoteku");
             return 1;
         }
     }
 
     int izbor;
     do {
-        printf("\n1. Unesi film\n2. Ispis svih filmova\n3. Ažuriraj film\n4. Obriši film\n5. Izlaz\n");
+        printf("\n1. Unesi film\n2. Ispis svih filmova\n3. Azuriraj film\n4. Obrisi film\n5. Izlaz\n");
         printf("Odaberite opciju: ");
         scanf("%d", &izbor);
-        getchar();  // Čisti buffer nakon unosa
+        getchar();
 
         switch (izbor) {
         case 1:
@@ -176,19 +178,26 @@ int main() {
             ispisFilmove(file);
             break;
         case 3:
-            azurirajFilm(file);
+            if (azurirajFilm()) {
+                fclose(file);
+                file = fopen("films.txt", "r+");
+            }
             break;
         case 4:
-            obrisiFilm(file);
+            if (obrisiFilm()) {
+                fclose(file);
+                file = fopen("films.txt", "r+");
+            }
             break;
         case 5:
             printf("Izlaz...\n");
             break;
         default:
-            printf("Nevažeća opcija!\n");
+            printf("Nevazeca opcija!\n");
         }
     } while (izbor != 5);
 
     fclose(file);
     return 0;
 }
+
