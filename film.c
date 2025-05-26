51d4de01dab05b982d2b5c22f7554df466f03430
DavidKordos <dkordos@etfos.hr>
2025-05-26 21:39:55 +0200

    Update film.c

--- film.c
+++ film.c
@@ -5,8 +5,8 @@
 #include "film.h"
 
 static const char* IME_DATOTEKE = "filmovi.txt";
+static const char* BACKUP_DATOTEKA = "filmovi_backup.txt";
 
-// Funkcija za prikaz izbornika
 void prikaziIzbornik() {
     printf("\n===== VIDEOTEKA =====\n");
     printf("1. Dodaj film\n");
@@ -14,12 +14,13 @@ void prikaziIzbornik() {
     printf("3. Azuriraj film\n");
     printf("4. Obrisi film\n");
     printf("5. Sortiraj filmove\n");
-    printf("6. Pretrazi film\n");
-    printf("7. Izlaz\n");
+    printf("6. Pretrazi film (binary search)\n");
+    printf("7. Ispis rekurzivno\n");
+    printf("8. Backup filmova\n");
+    printf("9. Izlaz\n");
     printf("Odaberi opciju: ");
 }
 
-// Funkcija za dodavanje filma
 void dodajFilm(Cvor** glava) {
     if (!glava) return;
     Cvor* novi = (Cvor*)malloc(sizeof(Cvor));
@@ -29,70 +30,73 @@ void dodajFilm(Cvor** glava) {
     }
 
     printf("Unesi ID: ");
-    scanf("%d", &((*novi).film.id));
+    scanf("%d", &(*novi).film.id);
     printf("Unesi naziv filma: ");
-    scanf(" %99[^\"]", (*novi).film.naziv);
+    scanf(" %99[^\n]", (*novi).film.naziv);
     printf("Unesi zanr: ");
-    scanf(" %49[^\"]", (*novi).film.zanr);
+    scanf(" %49[^\n]", (*novi).film.zanr);
     printf("Unesi trajanje (u minutama): ");
-    scanf("%f", &((*novi).film.trajanje));
+    scanf("%f", &(*novi).film.trajanje);
 
     (*novi).sljedeci = *glava;
     *glava = novi;
     spremiFilmove(*glava);
 }
 
-// Funkcija za prikaz svih filmova
 void prikaziFilmove(const Cvor* glava) {
     printf("\n--- Popis filmova ---\n");
     while (glava) {
-        printf("ID: %d | Naziv: %s | Zanr: %s | Trajanje: %.2f min\n",
+        PRINT_LINE;
+        printf("ID: %d\nNaziv: %s\nZanr: %s\nTrajanje: %.2f min\n",
             (*glava).film.id, (*glava).film.naziv, (*glava).film.zanr, (*glava).film.trajanje);
         glava = (*glava).sljedeci;
     }
 }
 
-// Funkcija za ažuriranje filma
+void ispisiRekurzivno(const Cvor* glava) {
+    if (!glava) return;
+    PRINT_LINE;
+    printf("ID: %d\nNaziv: %s\nZanr: %s\nTrajanje: %.2f min\n",
+        (*glava).film.id, (*glava).film.naziv, (*glava).film.zanr, (*glava).film.trajanje);
+    ispisiRekurzivno((*glava).sljedeci);
+}
+
 void azurirajFilm(Cvor* glava) {
-    int trazeniId;
+    int id;
     printf("Unesi ID filma za azuriranje: ");
-    scanf("%d", &trazeniId);
+    scanf("%d", &id);
 
     while (glava) {
-        if ((*glava).film.id == trazeniId) {
+        if ((*glava).film.id == id) {
             printf("Unesi novi naziv: ");
-            scanf(" %99[^\"]", (*glava).film.naziv);
+            scanf(" %99[^\n]", (*glava).film.naziv);
             printf("Unesi novi zanr: ");
-            scanf(" %49[^\"]", (*glava).film.zanr);
+            scanf(" %49[^\n]", (*glava).film.zanr);
             printf("Unesi novo trajanje: ");
-            scanf("%f", &((*glava).film.trajanje));
+            scanf("%f", &(*glava).film.trajanje);
             spremiFilmove(glava);
             return;
         }
         glava = (*glava).sljedeci;
     }
-    printf("Film s ID %d nije pronaðen.\n", trazeniId);
+    printf("Film nije pronaden.\n");
 }
 
-// Funkcija za brisanje filma
 void obrisiFilm(Cvor** glava) {
     if (!glava) return;
 
-    int trazeniId;
-    printf("Unesi ID filma za brisanje: ");
-    scanf("%d", &trazeniId);
+    int id;
+    printf("Unesi ID za brisanje: ");
+    scanf("%d", &id);
 
     Cvor* trenutni = *glava;
     Cvor* prethodni = NULL;
 
     while (trenutni) {
-        if ((*trenutni).film.id == trazeniId) {
-            if (prethodni == NULL) {
-                *glava = (*trenutni).sljedeci;
-            }
-            else {
-                (*prethodni).sljedeci = (*trenutni).sljedeci;
-            }
+        if ((*trenutni).film.id == id) {
+            if (!prethodni) *glava = (*trenutni).sljedeci;
+            else (*prethodni).sljedeci = (*trenutni).sljedeci;
+
             free(trenutni);
             spremiFilmove(*glava);
             printf("Film obrisan.\n");
@@ -101,10 +105,9 @@ void obrisiFilm(Cvor** glava) {
         prethodni = trenutni;
         trenutni = (*trenutni).sljedeci;
     }
-    printf("Film nije pronaðen.\n");
+    printf("Film nije pronaden.\n");
 }
 
-// Uèitavanje filmova iz datoteke
 void ucitajFilmove(Cvor** glava) {
     if (!glava) return;
 
@@ -114,12 +117,13 @@ void ucitajFilmove(Cvor** glava) {
         return;
     }
 
+    rewind(dat);
+
     Film f;
-    while (fscanf(dat, "%d,%99[^,],%49[^,],%f\n",
-        &f.id, f.naziv, f.zanr, &f.trajanje) == 4) {
-        Cvor* novi = (Cvor*)malloc(sizeof(Cvor));
+    while (fscanf(dat, "%d,%99[^,],%49[^,],%f\n", &f.id, f.naziv, f.zanr, &f.trajanje) == 4) {
+        Cvor* novi = malloc(sizeof(Cvor));
         if (!novi) {
-            perror("Greska pri alokaciji memorije");
+            perror("Greska alokacije");
             fclose(dat);
             return;
         }
@@ -130,49 +134,94 @@ void ucitajFilmove(Cvor** glava) {
     fclose(dat);
 }
 
-// Spremanje filmova u datoteku
 void spremiFilmove(const Cvor* glava) {
     FILE* dat = fopen(IME_DATOTEKE, "w");
     if (!dat) {
-        perror("Ne mogu spremiti filmove");
+        perror("Ne mogu spremiti");
         return;
     }
 
+    rewind(dat);
+
     while (glava) {
-        fprintf(dat, "%d,%s,%s,%.2f\n",
-            (*glava).film.id, (*glava).film.naziv, (*glava).film.zanr, (*glava).film.trajanje);
+        fprintf(dat, "%d,%s,%s,%.2f\n", (*glava).film.id, (*glava).film.naziv,
+            (*glava).film.zanr, (*glava).film.trajanje);
         glava = (*glava).sljedeci;
     }
     fclose(dat);
 }
 
-// Funkcija za sortiranje
+int compareFilmID(const void* a, const void* b) {
+    Film* fa = *(Film**)a;
+    Film* fb = *(Film**)b;
+    return (*fa).id - (*fb).id;
+}
+
+void pretraziFilmBsearch(Cvor* glava) {
+    int count = 0;
+    Cvor* temp = glava;
+    while (temp) {
+        count++;
+        temp = (*temp).sljedeci;
+    }
+
+    if (count == 0) {
+        printf("Nema filmova.\n");
+        return;
+    }
+
+    Film** niz = malloc(count * sizeof(Film*));
+    temp = glava;
+    for (int i = 0; i < count; i++) {
+        niz[i] = &(*temp).film;
+        temp = (*temp).sljedeci;
+    }
+
+    qsort(niz, count, sizeof(Film*), compareFilmID);
+
+    int trazeni;
+    printf("Unesi ID za pretragu: ");
+    scanf("%d", &trazeni);
+
+    Film key = { .id = trazeni };
+    Film* keyPtr = &key;
+
+    Film** pronadjen = bsearch(&keyPtr, niz, count, sizeof(Film*), compareFilmID);
+    if (pronadjen)
+        printf("Pronaden: %s (%s) %.2f min\n",
+            (*(*pronadjen)).naziv, (*(*pronadjen)).zanr, (*(*pronadjen)).trajanje);
+    else
+        printf("Film nije pronaden.\n");
+
+    free(niz);
+}
+
 int usporediFilmoveNaziv(const void* a, const void* b) {
-    const Film* f1 = *(const Film**)a;
-    const Film* f2 = *(const Film**)b;
+    Film* f1 = *(Film**)a;
+    Film* f2 = *(Film**)b;
     return strcmp((*f1).naziv, (*f2).naziv);
 }
 
 int usporediFilmoveTrajanje(const void* a, const void* b) {
-    const Film* f1 = *(const Film**)a;
-    const Film* f2 = *(const Film**)b;
+    Film* f1 = *(Film**)a;
+    Film* f2 = *(Film**)b;
     return ((*f1).trajanje > (*f2).trajanje) - ((*f1).trajanje < (*f2).trajanje);
 }
 
 void sortirajFilmove(Cvor** glava) {
     if (!glava || !*glava) return;
 
-    int broj = 0;
+    int count = 0;
     Cvor* temp = *glava;
     while (temp) {
-        broj++;
+        count++;
         temp = (*temp).sljedeci;
     }
 
-    Film** niz = (Film**)malloc(broj * sizeof(Film*));
+    Film** niz = malloc(count * sizeof(Film*));
     temp = *glava;
-    for (int i = 0; i < broj; i++) {
-        niz[i] = &((*temp).film);
+    for (int i = 0; i < count; i++) {
+        niz[i] = &(*temp).film;
         temp = (*temp).sljedeci;
     }
 
@@ -181,41 +230,42 @@ void sortirajFilmove(Cvor** glava) {
     scanf("%d", &nacin);
 
     if (nacin == 0)
-        qsort(niz, broj, sizeof(Film*), usporediFilmoveNaziv);
+        qsort(niz, count, sizeof(Film*), usporediFilmoveNaziv);
     else
-        qsort(niz, broj, sizeof(Film*), usporediFilmoveTrajanje);
+        qsort(niz, count, sizeof(Film*), usporediFilmoveTrajanje);
 
-    for (int i = 0; i < broj; i++)
+    for (int i = 0; i < count; i++) {
         printf("ID: %d | %s | %s | %.2f\n", (*niz[i]).id, (*niz[i]).naziv, (*niz[i]).zanr, (*niz[i]).trajanje);
+    }
 
     free(niz);
 }
-
-// Funkcija za pretraživanje
-void pretraziFilm(const Cvor* glava) {
-    int trazeniId;
-    printf("Unesi ID filma za pretragu: ");
-    scanf("%d", &trazeniId);
-
-    while (glava) {
-        if ((*glava).film.id == trazeniId) {
-            printf("Pronaðen: %s (%s) %.2f min\n", (*glava).film.naziv, (*glava).film.zanr, (*glava).film.trajanje);
-            return;
-        }
-        glava = (*glava).sljedeci;
-    }
-    printf("Film nije pronaðen.\n");
+void ispisiRekurzivno(const Cvor* glava) {
+    if (glava == NULL) return;
+    printf("ID: %d | Naziv: %s | Zanr: %s | Trajanje: %.2f min\n",
+        (*glava).film.id, (*glava).film.naziv,
+        (*glava).film.zanr, (*glava).film.trajanje);
+    ispisiRekurzivno((*glava).sljedeci);
 }
 
-// Oslobodi svu zauzetu memoriju
-void oslobodiMemoriju(Cvor** glava) {
-    if (!glava) return;
+void backupFilmova() {
+    if (rename(IME_DATOTEKE, BACKUP_DATOTEKA) == 0)
+        printf("Backup uspjesan (%s -> %s).\n", IME_DATOTEKE, BACKUP_DATOTEKA);
+    else
+        perror("Greska kod backup-a");
+}
 
+void oslobodiMemoriju(Cvor** glava) {
     while (*glava) {
         Cvor* temp = *glava;
         *glava = (*(*glava)).sljedeci;
         free(temp);
     }
 }
+
+
+
+
+
 
 
