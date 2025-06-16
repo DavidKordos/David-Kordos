93ac8d675db36d3eb7f9940074f480a07bafc9b2
David <dkordos@etfos.hr>
2025-06-17 00:51:12 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,53 +1,54 @@
 
-#define _CRT_SECURE_NO_WARNINGS
 
-#include <stdio.h>
-#include <stdlib.h>
+#define _CRT_SECURE_NO_WARNINGS 
+
+#include <stdio.h> 
+#include <stdlib.h> 
 #include "film.h"
 
 
-Cvor* glava = NULL;
+Cvor* glava = NULL;                   //Definira globalni pokazivač na prvi element povezane liste filmova
 
 int main() {
-    ucitajFilmove(&glava);
+    ucitajFilmove(&glava);          //Poziva funkciju za učitavanje filmova iz datoteke i inicijalizaciju liste
 
     int izbor;
-    do {
+    do {           
         prikaziIzbornik();
-        if (scanf("%d", &izbor) != 1) {
+        if (scanf("%d", &izbor) != 1) {               //Učitava korisnikov odabir u izborniku
             fprintf(stderr, "\nNeispravan unos.\n");
-            while (getchar() != '\n');
+            while (getchar() != '\n');  //Prikazuje izbornik i omogućuje korisniku izbor opcije sve dok ne odabere izlaz
             continue;
         }
 
-        switch ((IzbornikOpcije)izbor) {
+        switch ((IzbornikOpcije)izbor) {   //Prebacuje izvršavanje na funkciju ovisno o izboru korisnika
         case DODAJ_FILM:
-            dodajFilm(&glava);
+            dodajFilm(&glava); 
             break;
         case PRIKAZI_FILMOVE:
-            prikaziFilmove(glava);
+            prikaziFilmove(glava); 
             break;
         case AZURIRAJ_FILM:
-            azurirajFilm(glava);
+            azurirajFilm(glava); 
             break;
         case OBRISI_FILM:
-            obrisiFilm(&glava);
+            obrisiFilm(&glava); 
             break;
         case SORTIRAJ_FILMOVE:
-            sortirajFilmove(&glava);
+            sortirajFilmove(&glava); 
             break;
         case PRETRAZI_FILM:
             pretraziFilmBsearch(glava); 
             break;
         case REKURZIVNI_ISPIS:
-            ispisiRekurzivno(glava);
+            ispisiRekurzivno(glava); 
             break;
         case BACKUP_FILMOVA:
-            backupFilmova(glava);
+            backupFilmova(glava); 
             break;
         case IZLAZ:
-            spremiFilmove(glava);
-            oslobodiMemoriju(&glava);
+            spremiFilmove(glava); 
+            oslobodiMemoriju(&glava);    //Oslobađa svu dinamički alociranu memoriju prije izlaska
             printf("\nZatvaranje aplikacije...\n");
             break;
         default:
@@ -57,5 +58,6 @@ int main() {
 
     return 0;
 }
+
 
 
